31c2a78b5b4eb5fcac563d5fc29cb57348129ac7
Caleb <46455007+Yuppyboy@users.noreply.github.com>
2022-08-09 18:15:04 -0400

    Update classifier.py

--- ml/classifier.py
+++ ml/classifier.py
@@ -21,7 +21,7 @@ def process_video(f, face_detector, model):
     #                                   'format=gray|nv12,hwupload'],
     #                    pixelformat='vaapi_vld')
     for frame in iio.imread(f, index=None, extension='.mp4'):
-        frames.append(classify_frame(frame[:,:,0], face_detector, model))
+        frames.append(classify_frame(np.array(frame)[0], face_detector, model))
     frames = np.hstack(frames)
     print('shape of stack: ', frames.shape)
     # iio.imwrite(file, frames)
