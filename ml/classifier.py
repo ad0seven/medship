fd283b2528ea9b5e2b2e7ad351c64c2f3030b1c8
Caleb <46455007+Yuppyboy@users.noreply.github.com>
2022-08-10 09:34:40 -0400

    iter

--- ml/classifier.py
+++ ml/classifier.py
@@ -1,39 +1,17 @@
 import cv2
-import subprocess
 import numpy as np
 import tensorflow as tf
-import ffmpeg
 import imageio.v3 as iio
-import os
 
 def process_video(f, face_detector, model):
-    # vid = cv2.VideoCapture(f)
-    # w, h = vid.get(3), vid.get(4)
-    # file = 'tempfile.mp4'
-    # ffmpeg.output(f, file)
     frames = []
-    print('shape of video: ', iio.imread(f, index=None, extension='.mp4').shape)
-    # w = iio.get_writer('my_video.mp4', format='FFMPEG', mode='I', fps=1,
-    #                    codec='h264_vaapi',
-    #                    output_params=['-vaapi_device',
-    #                                   '/dev/dri/renderD128',
-    #                                   '-vf',
-    #                                   'format=gray|nv12,hwupload'],
-    #                    pixelformat='vaapi_vld')
-    for frame in iio.imread(f, index=None, extension='.mp4'):
+    for frame in iio.imiter(f, index=None, extension='.mp4'):
         frames.append(classify_frame(np.array(frame), face_detector, model))
     frames = np.stack(frames)
-    print('shape of stack: ', frames.shape)
-    # iio.imwrite(file, frames)
-    # subprocess.run(['chmod', '+x', 'ml/FaceLandmarkVid.exe'])
-    # subprocess.run(['ml/FaceLandmarkVid.exe', '-f', file, '-mloc', 'ml/main_clm_general.txt'])
-    # subprocess.run(['rm', file])
     return iio.imwrite("<bytes>", frames, extension=".mp4")
 
 def classify_frame(frame, face_detector, model):
 
-    # print(frame.shape)
-    
     emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     detected_faces = face_detector.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
@@ -68,11 +46,8 @@ def classify_frame(frame, face_detector, model):
             detect['height'] = str(h)
 
             face_prop.append(detect)
-            print(face_prop)
             
             cv2.putText(frame, label + " : " + str(confidence), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-
-    # print('new frame shape ', frame.shape)
         
     return frame
 
